7fcb33918621ab27fb6f1bc8bb1b09b3f97ac3ab
gagetstowers <38923134+gagetstowers@users.noreply.github.com>
2020-01-17 23:40:10 +0800

    Add files via upload

---
+++ captcha/captcha.js
@@ -0,0 +1,27 @@
+
+// SimpleCaptcha v1.0 © Anudeep Tubati under MIT License
+
+function ChangeCaptcha() {
+    var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
+
+    var string_length = 6;
+
+    var ChangeCaptcha = '';
+    for (var i=0; i<string_length; i++) {
+        var rnum = Math.floor(Math.random() * chars.length);
+        ChangeCaptcha += chars.substring(rnum,rnum+1);
+    }
+
+    document.getElementById('randomfield').value = ChangeCaptcha; // Final step which changes the field value to the Captcha produced
+}
+
+function check() {
+    if(document.getElementById('CaptchaEnter').value == document.getElementById('randomfield').value ) {
+
+        window.open('contactpage.html','_self');
+
+    }
+    else {
+        alert('Please re-check the captcha');
+    }
+}
